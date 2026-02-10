2f9ceaff8acfb4d53af83e870caa4c51100be322
Mehmet Altuğ JAKAL <98031676+crumbsws@users.noreply.github.com>
2026-02-10 22:17:31 +0000

    Force class

---
+++ src/core/force.py
@@ -0,0 +1,13 @@
+
+
+class Force:
+    def __init__(self, magnitude, direction):
+        assert type(magnitude) == int
+        assert type(direction) == tuple
+
+        self.magnitude = magnitude
+        self.direction = direction
+
+
+
+
